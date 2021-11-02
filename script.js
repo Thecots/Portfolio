0f9db6157d43d681338ac39ca46ae0528ef6c007
Thecots <thecots22@gmail.com>
2021-11-02 22:16:05 +0100

    idk

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-const David = {
-    nombre: 'David Cots Milla',
-    edad: 19,
-    estudios : [
-        {
-            type: "CFGM",
-            name: "Sistemas microinformáticos y Redes"
-        },
-        {
-            type: "CFGS",
-            name: "Desarrollo de Aplicaciónes Web"
-        }
-    ],
-    hobbies: [
-       'Entrenar',
-       'Programar',
-       'Fútbol',
-       'Música',
-       'Videojuegos',
-       'Series'
-    ]
-}
+var topButton = document.getElementById("topButton");
 
+
+// header Fixed
+window.onscroll = function() {
+    const docScrollTop = document.documentElement.scrollTop;
+    
+    if (window.innerWidth > 795) {
+        if(docScrollTop > 100) {
+            document.querySelector("header").classList.add("fixed");
+        }
+        else {
+            document.querySelector("header").classList.remove("fixed");
+        }
+    }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+      } else {
+        topButton.style.display = "none";
+      }
+};
+
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
