d35513a60c5382eddb5b4a9a00a8a45c317c7aca
Thecots <thecots22@gmail.com>
2021-11-02 13:54:07 +0100

    v0.0

---
+++ script.js
@@ -0,0 +1,22 @@
+const David = {
+    nombre: 'David Cots Milla',
+    edad: 19,
+    estudios : [
+        {
+            type: "CFGM",
+            name: "Sistemas microinformáticos y Redes"
+        },
+        {
+            type: "CFGS",
+            name: "Desarrollo de Aplicaciónes Web"
+        }
+    ],
+    hobbies: [
+       'Entrenar',
+       'Programar',
+       'Fútbol',
+       'Música',
+       'Videojuegos',
+       'Series'
+    ]
+}
